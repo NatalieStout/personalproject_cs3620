c861e583f42d6345dec5e8269509711c51ce2910
Natalie Stout <natalie_stout99@hotmail.com>
2021-03-17 21:31:12 -0600

    test

--- profile.php
+++ profile.php
@@ -76,7 +76,7 @@ if(!isset($_SESSION['loggedin'])){
                     <div class="content">' . $shows[$x]->getTvshowDate() . '</div>
                     
                     <button onClick="window.location.href=\'edit.php?id=' . $shows[$x]->getTvshowId() . '\'" class="button is-info">Edit
-                    <button onClick="window.location.href=\'delete.php?id=' . $shows[$x]->getTvshowId() . '\'" class="button is-danger">Delete
+                    <button onClick="window.location.href=\'edit.php?id=' . $shows[$x]->getTvshowId() . '\'" class="button is-info">Edit
                 </div>
             </div>';
     }
